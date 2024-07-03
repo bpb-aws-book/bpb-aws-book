6329b77cc104d56bee9833e1f23b70bf57e87726
bpb-aws-book <simplefusionmusic@gmail.com>
2024-07-02 23:52:46 -0700

    Update models.py

--- chapter2/inquisitive_bookworm_club/books/models.py
+++ chapter2/inquisitive_bookworm_club/books/models.py
@@ -6,7 +6,7 @@ class book(models.Model):
     description = models.TextField()
     author = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    is_rented = models.BooleanField(default=True)
+    is_rented = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
