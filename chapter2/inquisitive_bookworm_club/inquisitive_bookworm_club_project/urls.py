84582dc8c8e29d13ab94d1f35ff0ec4e0839b7b2
bpb-aws-book <simplefusionmusic@gmail.com>
2024-07-02 23:30:08 -0700

    Update urls.py

--- chapter2/inquisitive_bookworm_club/inquisitive_bookworm_club_project/urls.py
+++ chapter2/inquisitive_bookworm_club/inquisitive_bookworm_club_project/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path("details/<int:pk>/", views.details, name="details"),
     path("rentbook/<int:pk>/", views.rentbook, name="rentbook"),
     path("returnbook/<int:pk>/", views.returnbook, name="returnbook"),
-    path("about/", views.about, name="about")
+    path("about/", views.about, name="about")]
