9c2b5fa19e397c5cf60d926f63f8a4c6fa34a11d
bpb-aws-book <simplefusionmusic@gmail.com>
2025-01-01 22:48:53 -0800

    Create setup.sh

---
+++ chapter10/setup.sh
@@ -0,0 +1,1 @@
+python3 -m venv env && source env/bin/activate && python3 -m pip install --upgrade pip && python3 -m pip install django && python3 -m pip install django && python3 -m pip install gunicorn && deactivate
