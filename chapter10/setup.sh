6465f36e8ed417ac3bc74ef106d04173cc04a52f
bpb-aws-book <simplefusionmusic@gmail.com>
2025-01-03 23:12:04 -0800

    Update setup.sh

--- chapter10/setup.sh
+++ chapter10/setup.sh
@@ -1,9 +1,7 @@
 python3 -m venv env && \
 source env/bin/activate && \
 python3 -m pip install --upgrade pip && \
-python3 -m pip install django && \
-python3 -m pip install gunicorn && \
-python3 -m pip install boto3 psycopg2-binary aws-secretsmanager-caching && \
+pip install -r requirements.txt && \
 dnf install -y postgresql15 postgresql15-contrib && \
 chmod -R a+rwx env && \
 deactivate
