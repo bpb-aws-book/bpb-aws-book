80a4cf14dbcb823fb02e600a0fe4d41694b14b00
bpb-aws-book <simplefusionmusic@gmail.com>
2025-06-15 00:56:59 -0700

    Update build_layer.sh

--- chapter15/build_layer.sh
+++ chapter15/build_layer.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
-# Script to build the Lambda layer with psycopg2
+# Script to build the Lambda layer with psycopg2 for Python 3.12
 
 # Create directory structure
 mkdir -p ./layer/python
 
-# Install dependencies into the layer directory
-pip install -r ./src/python/requirements.txt -t ./layer/python
+# Use Docker to build in Amazon Linux 2 environment
+docker run --rm -v $(pwd):/var/task amazon/aws-lambda-python:3.12 \
+  /bin/bash -c "
+    # Install build dependencies
+    yum install -y postgresql-devel gcc python3-devel
+    
+    # Install psycopg2-binary into the layer directory
+    pip install psycopg2-binary -t /var/task/layer/python
+    
+    # Fix permissions
+    chown -R $(id -u):$(id -g) /var/task/layer
+  "
+
+# Create zip file for the layer
+cd layer
+zip -r ../psycopg2-layer.zip .
+cd ..
 
 # Copy the layer to the src directory
-cp -r ./layer/* ./src/
+mkdir -p ./src/python
+cp -r ./layer/python/* ./src/python/
 
-echo "Layer built successfully"
+echo "Layer built successfully at $(pwd)/psycopg2-layer.zip"
+echo "Layer contents also copied to ./src/python/ for SAM deployment"
