9ede7d79806d119d655febe80223c32e6f049e49
bpb-aws-book <simplefusionmusic@gmail.com>
2025-06-15 01:56:37 -0700

    Update build_layer.sh

--- chapter15/build_layer.sh
+++ chapter15/build_layer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Script to build the Lambda layer with psycopg2 for Python 3.12
+# Script to build the Lambda layer with pg8000 for Python 3.12
 
 # Create directory structure
 mkdir -p ./layer/python
@@ -7,11 +7,8 @@ mkdir -p ./layer/python
 # Use Docker to build in Amazon Linux 2 environment
 docker run --rm -v $(pwd):/var/task amazon/aws-lambda-python:3.12 \
   /bin/bash -c "
-    # Install build dependencies
-    yum install -y postgresql-devel gcc python3-devel
-    
-    # Install psycopg2-binary into the layer directory
-    pip install psycopg2-binary -t /var/task/layer/python
+    # Install pg8000 into the layer directory
+    pip install pg8000 -t /var/task/layer/python
     
     # Fix permissions
     chown -R $(id -u):$(id -g) /var/task/layer
@@ -19,12 +16,12 @@ docker run --rm -v $(pwd):/var/task amazon/aws-lambda-python:3.12 \
 
 # Create zip file for the layer
 cd layer
-zip -r ../psycopg2-layer.zip .
+zip -r ../pg8000-layer.zip .
 cd ..
 
 # Copy the layer to the src directory
 mkdir -p ./src/python
 cp -r ./layer/python/* ./src/python/
 
-echo "Layer built successfully at $(pwd)/psycopg2-layer.zip"
+echo "Layer built successfully at $(pwd)/pg8000-layer.zip"
 echo "Layer contents also copied to ./src/python/ for SAM deployment"
