1f2cc836fd661305a3151a7db94475fdc6867070
bpb-aws-book <simplefusionmusic@gmail.com>
2025-06-18 15:46:34 -0700

    Delete chapter15/executesleeplambdafunction.sh

--- chapter15/executesleeplambdafunction.sh
+++
@@ -1,8 +0,0 @@
-# Run 5 invocations to cause throttling (since concurrency is set to 4)
-for i in {1..5}; do
-  aws lambda invoke \
-    --function-name test \
-    --invocation-type Event \
-    --payload '{}' \
-    output$i.txt &
-done
