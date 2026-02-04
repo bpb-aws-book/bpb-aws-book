8a508c56d44fa93bd19fd58e9e458e4d67405c84
bpb-aws-book <simplefusionmusic@gmail.com>
2026-02-04 00:08:01 -0800

    Update updatedfunctionstep2.py

--- chapter24/updatedfunctionstep2.py
+++ chapter24/updatedfunctionstep2.py
@@ -2,9 +2,11 @@ import pandas as pd
 import boto3
 
 def lambda_handler(event, context):
-s3 = boto3.client('s3')
-obj = s3.get_object(Bucket='your-bucket-name', Key='sampledatafile.csv')
-df = pd.read_csv(obj['Body'])
 
-author = df[df['book'] == 'Code Complete']['author'].values[0]
-print(author)
+    s3 = boto3.client('s3')
+    obj = s3.get_object(Bucket='ch2ch2-chapter24bucket-l4n4oqwlonql', Key='sampledatafile.csv')
+    df = pd.read_csv(obj['Body'])
+
+    author = df[df['book'] == 'Code Complete']['author'].values[0]
+    print(author)
+    return {'statusCode': 200, 'body': author}
