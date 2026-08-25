edeb3f81107e656d60ab3fc71f268278ee3bf037
bpb-aws-book <simplefusionmusic@gmail.com>
2026-08-24 18:37:49 -0700

    Update database_script.sql

--- chapter6/database_script.sql
+++ chapter6/database_script.sql
@@ -6,7 +6,7 @@ psql --host=<database endpoint> --port=5432 --dbname=<databasename> --username=<
 /*perform a select*/
 Select * from "books_book";
 
-/* Add a test record*?
+/* Add a test record*/
 INSERT INTO "books_book" 
 ("id","name", "description", "author", "price", "is_rented", "created_at", "updated_at")
 values
